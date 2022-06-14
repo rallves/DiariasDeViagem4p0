05a22a3d7904529598d54ce790f892970db891f7
Renato Almeida <rallves@gmail.com>
2022-06-14 17:42:08 -0300

    Commit Inicial - Renato Almeida - Inclusao de classes Testes em Eventos de Campos e Tarefas de Serviços

---
+++ src/br/srv/mgs/flow/DiariasDeViagem/EventosDeCampo/Teste.java
@@ -0,0 +1,11 @@
+package br.srv.mgs.flow.DiariasDeViagem.EventosDeCampo;
+
+import br.com.sankhya.extensions.flow.ContextoEvento;
+import br.com.sankhya.extensions.flow.EventoProcessoJava;
+
+public class Teste implements EventoProcessoJava {
+    @Override
+    public void executar(ContextoEvento contextoEvento) throws Exception {
+
+    }
+}
